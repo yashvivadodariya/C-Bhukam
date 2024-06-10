e445fbe723dade15ddf4933328621de7002fc27c
yashvivadodariya <yashvivadodariya72@gmail.com>
2024-06-10 13:48:28 +0530

    10/6/24

---
+++ ch-4/4_1/EXPLICIT.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+       int a= 34;
+       float pi = 3.14;
+
+       clrscr();
+
+
+
+       printf("A\t : %d\n",a);
+       printf("A\t : %f",(float)a);
+
+
+
+       printf("pi\t : %2.f\n",pi);
+       printf("pi\t : %d",(int)pi);
+
+
+
+
+       getch();
+
+
+
+}
---
+++ ch-4/4_1/IMPLICIT.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+       int a= 94;
+       char ch = 'y';
+
+       clrscr();
+
+
+
+       printf("A\t : %d\n",a);
+       printf("A\t : %c",a);
+
+
+
+       printf("ch\t : %c\n",ch);
+       printf("ch\t : %d",ch);
+
+
+
+
+       getch();
+
+
+
+}
